833bef48f2c41cda85c31f224f1b9c2472d3d2e2
Leandro <lean.arrigonni@gmail.com>
2023-01-21 19:26:11 -0300

    Tailwind agregado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Index from './pages/Index'
+import '../styles/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
 )

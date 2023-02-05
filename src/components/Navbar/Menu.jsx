03fbcab98b99e2481c56ded212bcc7ab4f89f95d
Leandro <lean.arrigonni@gmail.com>
2023-02-04 22:03:24 -0300

    Cambio en el icono de menu

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { ReactComponent as Bars } from  "../../../assets/bars-solid.svg";
+import { ReactComponent as Bars } from "../../../assets/bars-solid.svg";
 
 const Menu = () => {
   return (
-    <div className="navbar-menu">
-      <Bars className="bars-logo"/>
-    </div>
+
+          <button className="bars-logo">
+          <Bars />
+        </button>
   );
 };
 
